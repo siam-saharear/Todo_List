3c519a5fb951fb93f708fe0be344edae0c22f55c
Zeher <siamsaharear010@gmail.com>
2025-05-23 21:46:16 +0600

    migration changes

--- objectives/models.py
+++ objectives/models.py
@@ -17,7 +17,7 @@ class Task(models.Model):
     uuid = models.UUIDField(default=uuid4, primary_key=True, verbose_name="task uuid")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null= True)
     name = models.CharField(max_length=50, verbose_name="task name")
-    descripiton = models.CharField(max_length=100, null=True, blank=False, verbose_name="task description")
+    descripiton = models.CharField(max_length=100, null=True, blank=True, verbose_name="task description")
     creation_time = models.DateTimeField(auto_now_add=True)
     type = models.CharField(max_length=30, choices=[("exercise", "Exercise"),("chores", "Chores"),("academic", "Academic"),("code", "Code"),("habit","Habit"),("other","Other")], default="NA")
     status = models.BooleanField(default=False)
